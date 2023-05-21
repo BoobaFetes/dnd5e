0a5da86b31f59cab4479fcad9a09a3f66bc5e960
Axel RG <rg_axel@yahoo.fr>
2023-05-21 17:37:27 +0200

    save work

--- workspace/libs/dnd5e-application/src/lib/Hero/HeroItem.tsx
+++ workspace/libs/dnd5e-application/src/lib/Hero/HeroItem.tsx
@@ -20,21 +20,21 @@ export const HeroItem: FC<IHeroItemProps> = memo(({ hero, onEdit }) => {
         </Grid>
         <Grid item xs={12} md={8}>
           <Box sx={{ display: 'flex' }}>
-            <Typography>Name :</Typography>
-            <Typography>{hero.name}</Typography>
+            <Typography sx={{ marginRight: 1 }}>Name :</Typography>
+            <Typography> {hero.name}</Typography>
           </Box>
           <Box sx={{ display: 'flex' }}>
-            <Typography>Race :</Typography>
-            <Typography>{hero.race}</Typography>
+            <Typography sx={{ marginRight: 1 }}>Race :</Typography>
+            <Typography> {hero.race}</Typography>
           </Box>
           <Box sx={{ display: 'flex' }}>
-            <Typography>Class :</Typography>
-            <Typography>{hero.class}</Typography>
+            <Typography sx={{ marginRight: 1 }}>Class :</Typography>
+            <Typography> {hero.class}</Typography>
           </Box>
         </Grid>
         <Grid item xs={12}>
-          <Typography>Abilities :</Typography>
-          <Typography>{hero.class}</Typography>
+          <Typography sx={{ marginRight: 1 }}>Abilities :</Typography>
+          <Typography> {hero.class}</Typography>
         </Grid>
       </Grid>
     </Grid>
