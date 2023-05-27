dde6ad18a0a746bedeccc6bd773d907d490f70e0
Axel RG <rg_axel@yahoo.fr>
2023-05-27 04:39:42 +0200

    save works

--- workspace/apps/client/src/app/Hero/HeroItem.tsx
+++ workspace/apps/client/src/app/Hero/HeroItem.tsx
@@ -1,5 +1,13 @@
 import { ICharacter } from '@boobafetes/dnd5e-domain';
-import { Box, Divider, Grid, Typography } from '@mui/material';
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Divider,
+  Grid,
+  Typography,
+} from '@mui/material';
 import { FC, memo } from 'react';
 import { HeroAbilities } from './HeroAbilities';
 import { HeroImage } from './HeroImage';
@@ -37,9 +45,62 @@ export const HeroItem: FC<IHeroItemProps> = memo(({ hero }) => {
           </Box>
         </Grid>
         <Grid item xs={12} sx={{ marginTop: 2 }}>
-          <Typography sx={{ marginRight: 1, textDecoration: 'underline' }}>
-            Equipements :
-          </Typography>
+          <Accordion>
+            <AccordionSummary>Equipements</AccordionSummary>
+            <AccordionDetails>
+              <Grid container>
+                <Grid item container xs={12} md={4} direction="column">
+                  <Typography>Armors</Typography>
+
+                  <ul>
+                    {hero.equipement.armors.map(
+                      ({ index, name, armor_class }) => {
+                        return (
+                          <li
+                            key={index}
+                          >{`${name} (base ${armor_class.base})`}</li>
+                        );
+                      }
+                    )}
+                  </ul>
+                </Grid>
+                <Grid item container xs={12} md={4} direction="column">
+                  <Typography>Melee</Typography>
+                  <ul>
+                    {hero.equipement.melees.map(
+                      ({ index, name, damage, two_handed_damage }) => {
+                        return (
+                          <li key={index}>
+                            {name}
+                            {` - damage : ${damage?.damage_dice}${
+                              (!!two_handed_damage &&
+                                ` (${two_handed_damage.damage_dice})`) ||
+                              ''
+                            }`}
+                          </li>
+                        );
+                      }
+                    )}
+                  </ul>
+                </Grid>
+                <Grid item container xs={12} md={4} direction="column">
+                  <Typography>Ranged</Typography>
+                  <Typography>
+                    {!!hero.equipement.ranged && (
+                      <ul>
+                        <li>
+                          {hero.equipement.ranged?.name}
+                          {` - damage : ${hero.equipement.ranged?.damage?.damage_dice}`}
+                        </li>
+                      </ul>
+                    )}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </AccordionDetails>
+          </Accordion>
+        </Grid>
+        <Grid item sx={{ marginTop: 2 }}>
           <Typography>{`Gold : ${hero.gold}`}</Typography>
         </Grid>
       </Grid>
