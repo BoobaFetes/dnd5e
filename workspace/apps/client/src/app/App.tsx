b091efdc2645b7212aa21a93c332cff964e418ff
Axel RG <rg_axel@yahoo.fr>
2023-05-29 23:48:50 +0200

    remove navigation button to "home"

--- workspace/apps/client/src/app/App.tsx
+++ workspace/apps/client/src/app/App.tsx
@@ -21,7 +21,7 @@ export function App() {
       <Grid item className="app-bar">
         <ResponsiveAppBar
           pages={[
-            { label: 'Home', to: '/' },
+            // rien à mettre dans le home pour le moment { label: 'Home', to: '/' },
             { label: 'Heroes', to: '/hero' },
             { label: 'Races', to: '/race' },
             { label: 'Classes', to: '/class' },
